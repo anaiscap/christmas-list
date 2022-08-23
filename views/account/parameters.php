695d7c52efb45077201a659ba98c07580e2b9955
anaiscap <anais.capra@3wa.io>
2022-08-23 10:58:12 +0200

    css parameters

--- views/account/parameters.php
+++ views/account/parameters.php
@@ -1,12 +1,12 @@
-<section class="accueil">
+<section class="accueil parameters">
 
     <?php if(isset($_SESSION['user'])): ?>
         <div class="account-frame">
             <img class="avatar-img" src="<?= $avatarsrc['avatar_src'] ?>" alt="avatar-utilisateur">
             <h1 class="center"><span> <?= $users['first_name'] ?> <?= $users['last_name'] ?></span></h1>
         </div>
-        <form class="login" method="POST">
-
+        <form class="sign-up" method="POST">
+        
         <fieldset class="avatar-fieldset">
             <legend>Avatar</legend>
             <div class="avatar-signup">
@@ -18,19 +18,22 @@
                 <?php endforeach; ?>
                 </div>
         </fieldset>
-        <fieldset>
-            <label for="first_name">Prénom</label>
-            <input name="first_name" id="first_name" type="text" value="<?= $users['first_name'] ?>">
-            <label for="last_name">Nom</label>
-            <input name="last_name" id="last_name" type="text" value="<?= $users['last_name'] ?>">
-            <label for="email">Adresse email</label>
-            <input name="email" id="email" type="email" value="<?= $users['email'] ?>">
+        <div class="avatar-fieldset">
+        <h2>Paramètres du compte</h2>
+        <fieldset >
+            <p><label for="first_name">Prénom</label>
+            <input name="first_name" id="first_name" type="text" value="<?= $users['first_name'] ?>"></p>
+            <p><label for="last_name">Nom</label>
+            <input name="last_name" id="last_name" type="text" value="<?= $users['last_name'] ?>"></p>
+            <p><label for="email">Adresse email</label>
+            <input name="email" id="email" type="email" value="<?= $users['email'] ?>"></p>
             <input name="password" id="password" type="hidden" value="<?= $users['password'] ?>">
-        </fieldset>
             <button class="gray-btn" type="submit">Enregistrer</button>
+        </fieldset>
+        </div>
         </form>
     <?php endif; ?>     
 
-    <a href="newpassword">Changer le mot de passe</a>
+    <a class="green-btn center" href="newpassword">Changer le mot de passe</a>
 
 </section>
